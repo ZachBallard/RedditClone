3b39bc77a4e5f9ef323a45ff261bd913c5fb702f
Zach Ballard <zbginji@gmail.com>
2016-04-14 10:11:24 -0500

    added title/comment expansion after sidebar and reworked initial comment section

--- RedditClone/Migrations/Configuration.cs
+++ RedditClone/Migrations/Configuration.cs
@@ -29,10 +29,16 @@ namespace RedditClone.Migrations
             //    );
             //
             context.RedditPosts.AddOrUpdate(
-            p => p.Title,
+            p => p.Author,
             new RedditPost {Author ="Zach Ballard", PostTime = new DateTime(2016,1,1), Title="I am a great title!", Body="Blah blah blah", ImageUrl= "https://www.fillmurray.com/150/100" },
             new RedditPost { Author = "Frank Fragglestack", PostTime = new DateTime(2016, 2, 1), Title = "I am a better title!", Body = "Blah blah blah", ImageUrl = "https://www.fillmurray.com/150/100" },
-            new RedditPost { Author = "Brutisy Banjopants", PostTime = new DateTime(2016, 3, 1), Title = "I AM YELLING!", Body = "BLAH BLAH BLAH", ImageUrl = "https://www.fillmurray.com/150/100" }
+            new RedditPost { Author = "Brutisy Banjopants", PostTime = new DateTime(2016, 3, 1), Title = "I AM YELLING!", Body = "BLAH BLAH BLAH", ImageUrl = "https://www.fillmurray.com/150/100" },
+            new RedditPost { Author = "Brutisy Banjopants", PostTime = new DateTime(2016, 3, 1), Title = "I am a repeat.", Body = "BLAH BLAH BLAH", ImageUrl = "https://www.fillmurray.com/150/100" },
+            new RedditPost { Author = "Brutisy Banjopants", PostTime = new DateTime(2016, 3, 1), Title = "Also a repeat", Body = "BLAH BLAH BLAH", ImageUrl = "https://www.fillmurray.com/150/100" },
+            new RedditPost { Author = "Brutisy Banjopants", PostTime = new DateTime(2016, 3, 1), Title = "Also a repeat", Body = "BLAH BLAH BLAH", ImageUrl = "https://www.fillmurray.com/150/100" },
+            new RedditPost { Author = "Brutisy Banjopants", PostTime = new DateTime(2016, 3, 1), Title = "Also a repeat", Body = "BLAH BLAH BLAH", ImageUrl = "https://www.fillmurray.com/150/100" },
+            new RedditPost { Author = "Brutisy Banjopants", PostTime = new DateTime(2016, 3, 1), Title = "Also a repeat", Body = "BLAH BLAH BLAH", ImageUrl = "https://www.fillmurray.com/150/100" },
+            new RedditPost { Author = "Brutisy Banjopants", PostTime = new DateTime(2016, 3, 1), Title = "This is a really long title to show how it now stretches across the page when under the sidebar! It is neat.", Body = "BLAH BLAH BLAH", ImageUrl = "https://www.fillmurray.com/150/100" }
             );
         }
     }
